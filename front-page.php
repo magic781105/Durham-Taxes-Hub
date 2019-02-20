3d6eafb5ffc1e6f5bea8082d5f1576a305ffe9b3
Anmol <anmolsingh9898@gmail.com>
2019-02-20 09:36:38 -0500

    volunteers and empowerment page ready

--- front-page.php
+++ front-page.php
@@ -15,86 +15,142 @@
 get_header();
 ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+<div id="primary" class="content-area">
+
+    <main id="volunteersMain" class="site-main">
+
+        <?php
+			// does get field exist
+			if (function_exists('get_field')){
+				
+				$volunteers = get_field('volunteers');
+		?>
+
+
+        <section class="volunteers grid-container">
+            <?php 
+						$volunteer_header = $volunteers["header"];
+						$volunteer_volunteertext1 = $volunteers["volunteertext1"];
+						$volunteer_volunteerVideo = $volunteers["volunteerVideo"];	
+						$volunteer_volunteerVideoDesc = $volunteers["volunteervideodesc"];		
+						$volunteer_volunteertext2 = $volunteers["volunteertext2"];						
+						?>
+
+            <h1 class="volunteerHeader">
+                <?php echo $volunteer_header; ?>
+            </h1>
+            <div class="volunteerContainer grid-x grid-margin-x">
+
+                <div class="volunteerTextSection cell large-4">
+                    <h1>How to become a volunteer </h1>
+                    <p class="volunteerText1">
+                        <?php echo $volunteer_volunteertext1; ?>
+                    </p>
+                    <a>Click here >></a>
+                </div>
+
+                <div class="volunteerVideo cell small-8 medium-6 large-4">
+
+                    <div class="volunteerVideo1"><img src="<?php echo $volunteer_volunteerVideo;?>" alt="video"></div>
+
+                    <p class="volunteerVideoDesc">
+                        <?php echo $volunteer_volunteerVideoDesc;?>
+                    </p>
+                </div>
+
+                <div class="volunteerTextSection cell large-4">
+                    <h1>How to become a volunteer </h1>
+                    <p class="volunteerText1">
+                        <?php echo $volunteer_volunteertext1; ?>
+                    </p>
+                    <a>Click here >></a>
+                </div>
+            </div>
+
+
+            <?php
+			}
+		?>
+
+          
+        </section><!-- #primary -->
+    </main><!-- #main -->
+
+	<main id="empowermentMain" class="site-main">
 
 		<?php
 			// does get field exist
 			if (function_exists('get_field')){
-				$featured_slider = get_field('featured_slider');
-				$testmonials = get_field('testmonials');
-				?>
 				
-				<div class="slider">
-					<?php
-					// var_dump($featured_slider);
-					foreach($featured_slider as $featured_slide) {
-						$header = $featured_slide['heading'];
-						$content = $featured_slide['content'];
-						$image = $featured_slide['image'];
-						//var_dump($header);
+				$empowerment = get_field('financialempowerment');
+		?>
+
+
+		<section class="empowerment grid-container">
+			<?php 
+						$empowerment_header = $empowerment["header"];
+						$empowerment_listItems = $empowerment["empowermentlist"];
+						$empowerment_text1 = $empowerment["empowermenttext1"];
+						$empowerment_text2 = $empowerment["empowermenttext2"];
+						$empowerment_listitem1 = $empowerment_listItems["listitem1"];
+						$empowerment_listitem2 = $empowerment_listItems["listitem2"];
+						$empowerment_listitem3 = $empowerment_listItems["listitem3"];
+						$empowerment_listitem4 = $empowerment_listItems["listitem4"];
+						$empowerment_listitem5 = $empowerment_listItems["listitem5"];
+										
 						?>
 
-						<div class="slide" style="background-image:url(<?php echo $image['url'];?>);">
-							<h1><?php echo $header; ?></h1>
-							<p><?php echo $content; ?></p>
-						</div>
-						<?php
-					}
-					?>
-				</div>
-				<div class="testmonial">
-					<?php 
-						$testmonial_header = $testmonials["header"];						
-						?>
-						
-						<p class="sectionHeader"><?php echo $testmonial_header; ?></p>
-						<div id="allTestmonial">
-						<?php 
+			<h1 class="empowermentHeader">
+				<?php echo $empowerment_header; ?>
+			</h1>
+			<div class="empowermentContainer grid-x grid-margin-x">
+				
+				
+				<div class="listContain cell small-12 medium-8 large-3">
+					<ol>
+						<li class ="listItem">
+							<?php echo $empowerment_listitem1; ?>
+						</li>
 
-						$i = 0;
-						foreach($testmonials as $testmonial){
-							if($i > 0){
-							$testmonial_img = $testmonial['image'];
-							$testmonial_content = $testmonial['content'];
-							$testmonial_name = $testmonial['name'];
-							// var_dump($testmonial_img);						
-							?>
-							
-							<div class="testmonialGroup">								
-								<div class="testmonialImage" style="background-image:url(<?php echo $testmonial_img['url'];?>);"></div>
-								<div class="testmonialContent"><?php echo $testmonial_content; ?></div>
-								<div class="testmonialName"><?php echo $testmonial_name; ?></div>
-							</div>
-							
-							<?php	
-							}
-							$i++;						
-						}
-						?>
-						</div>
-					
+						<li class ="listItem">
+							<?php echo $empowerment_listitem2; ?>
+						</li>
+
+						<li class ="listItem">
+							<?php echo $empowerment_listitem3; ?>
+						</li>
+
+						<li class ="listItem">
+							<?php echo $empowerment_listitem4; ?>
+						</li>
+
+						<li class ="listItem">
+							<?php echo $empowerment_listitem5; ?>
+						</li>
+					</ul>
 				</div>
-				<?php
+				<div class="listContainBg cell small-12 medium-8 large-7">
+					<div class="listResult ">
+						<h2>Overview</h2>
+						<p><?php echo $empowerment_text1; ?></p>
+						<p><?php echo $empowerment_text2; ?></p>
+					</div>
+				</div>
+
+			
+				
+			</div>
+
+
+			<?php
 			}
 		?>
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+			
+	</section><!-- #primary -->
+</main><!-- #main -->
 
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+</div>
 
 <?php
 // get_sidebar();
