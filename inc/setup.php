f22b8df6749bedad8827343ea7f389e8a5bb2403
Anmol <anmolsingh9898@gmail.com>
2019-03-08 19:36:25 -0500

    responsive volunteer section added

--- inc/setup.php
+++ inc/setup.php
@@ -49,7 +49,7 @@ if ( ! function_exists( 'durhamtaxhub_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'durhamtaxhub' ),
+			'primary-menu' => esc_html__( 'Primary menu', 'durhamtaxhub' ),
 		) );
 
 		/*
