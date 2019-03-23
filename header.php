7b387e20e49ac1d267a828753c9c1137ba571a3a
Anmol <anmolsingh9898@gmail.com>
2019-03-23 16:00:03 -0400

    comments added in navigation

--- header.php
+++ header.php
@@ -12,13 +12,14 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<link href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" rel="stylesheet">
 
-	<?php wp_head(); ?>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <link href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" rel="stylesheet">
+
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
@@ -26,18 +27,18 @@
         <a class="skip-link screen-reader-text" href="#content">
             <?php esc_html_e( 'Skip to content', 'durhamtaxhub' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
+        <header id="masthead" class="site-header">
+            <div class="site-branding">
+                <?php
 			the_custom_logo();
 			if ( is_front_page() && is_home() ) :
 				?>
-				<!-- <h1 class="site-title"><a href="<?php //echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php //bloginfo( 'name' ); ?></a></h1> -->
-				<?php
+                <!-- <h1 class="site-title"><a href="<?php //echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php //bloginfo( 'name' ); ?></a></h1> -->
+                <?php
 			else :
 				?>
-				<!-- <p class="site-title"><a href="<?php //echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php //bloginfo( 'name' ); ?></a></p> -->
-				<?php
+                <!-- <p class="site-title"><a href="<?php //echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php //bloginfo( 'name' ); ?></a></p> -->
+                <?php
 			endif;
 			$durhamtaxhub_description = get_bloginfo( 'description', 'display' );
 			if ( $durhamtaxhub_description || is_customize_preview() ) :
@@ -45,13 +46,21 @@
                 <!-- <p class="site-description"><?php //echo $durhamtaxhub_description; /* WPCS: xss ok. */ ?></p> -->
                 <?php endif; ?>
             </div><!-- .site-branding -->
+            <!--        nav container-->
             <div id="menu-bar">
+
+                <!--                navigation bar wrap-->
                 <nav id="site-navigation" class="main-navigation">
+
+                    <!--                    navigation click to slide nav bar in and out-->
                     <div id="navigation" data-click-state="0"></div>
+
 
                     <div class="nav-menu">
                         <div class="nav-border">
                             <?php
+                            
+                            //wp nav function to get navigation items
 								wp_nav_menu( array(
 								'theme_location' => 'primary-menu',
 								'menu_id'        => 'primary-menu',
@@ -63,4 +72,4 @@
             </div><!-- #site-navigation -->
         </header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+        <div id="content" class="site-content">
